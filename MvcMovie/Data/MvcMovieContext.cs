700358027db0e0b8222e652ccd1ba4b896f74da0
Ichwan.ElHamdhani <boootx@gmail.com>
2023-09-08 16:03:10 +0700

    Add project files.

---
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+              public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
